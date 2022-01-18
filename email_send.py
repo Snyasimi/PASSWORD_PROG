6de517a1e694540663467b094ee1bf2b2bb391cf
S.Nyasimi <89997376+Snyasimi@users.noreply.github.com>
2022-01-18 08:11:49 +0300

    Create email_send.py
    for sending emails using python

---
+++ email_send.py
@@ -0,0 +1,18 @@
+"""this codde is not yet complete"""
+
+
+
+import smtplib
+from email.message import EmailMessage
+
+with smtplib.SMTP_SSL("smtp.gmail.com ,465) as SMTP :
+                      
+
+
+
+msg = EmailMessage()
+msg["Subject"] = 
+msg["From"] =
+msg["to"] = 
+msg.setContent("string)
+               
