cb53c23f50145b2e5c81e03acd99f0f32adb7c97
S.Nyasimi <89997376+Snyasimi@users.noreply.github.com>
2022-01-19 11:48:47 +0300

    complete email send

--- email_send.py
+++ email_send.py
@@ -4,15 +4,28 @@
 
 import smtplib
 from email.message import EmailMessage
+#set password and acc to use 
+
+EMAIL_PASSOWRD = "use env variable'
+EMAIL_ADRESS = "set env variable"
+
 
 with smtplib.SMTP_SSL("smtp.gmail.com ,465) as SMTP :
+#secures,encrypts a connecton
                       
 
 
 
 msg = EmailMessage()
-msg["Subject"] = 
-msg["From"] =
-msg["to"] = 
-msg.setContent("string)
+msg["Subject"] = "input txt"
+msg["From"] ="input"
+                    
+msg["to"] = "input'
+msg.set_content("string)
                
+
+with smtplib.SMTP_SSL("smtp.gmail.com",465) as smtp:
+                #login to your gmail
+                smtp.login("*email adress here","*paswd")
+                #send email
+                smtp.send_message(msg)
